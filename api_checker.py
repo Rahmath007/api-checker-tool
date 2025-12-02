9849a436b7b073e0c6a42da54d72b5c0d1eb23fe
RAHMATH MOZUMDER <rahmath207@gmail.com>
2025-12-02 14:27:11 +0000

    Formatted code

--- api_checker.py
+++ api_checker.py
@@ -1,35 +1,25 @@
 import requests
 
-# url = "https://api.github.com"
-
-# repsonse = requests.get(url)
-
-# print("Status Code:",repsonse.status_code)
-
-
 def check_api(url):
     try:
-        repsonse = requests.get(url, timeout=5)
-        print("URL:", url)
-        print("Status Code:", repsonse.status_code)
-        
-        
-        if repsonse.status_code == 200:
-            print("API is working properly.")
+        response = requests.get(url, timeout=5)
+        print("\nURL:", url)
+        print("Status code:", response.status_code)
+
+        if response.status_code == 200:
+            print("API is working ✅")
         else:
-            print("API returned an error.")
-            
-    
+            print("API returned a problem ⚠️")
+
     except requests.exceptions.Timeout:
-        print("Error: The request timed out.")
+        print("Request timed out ⏱")
     except requests.exceptions.ConnectionError:
-        print("Error: Could not connect to the API.")
+        print("Connection error ❌")
     except Exception as e:
-        print("An unexpected error occurred:") 
-    
-    
+        print("Unexpected error:", e)
+
 if __name__ == "__main__":
-    url = input("Enter the API URL to check: ")
+    url = input("Enter API URL: ")
     check_api(url)
     
     
